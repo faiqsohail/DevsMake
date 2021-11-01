9d7f484d46b3203c996f83aa6476b436fadd850b
Faiq Sohail <me@faiqsohail.com>
2021-11-01 14:26:56 -0600

    Created posts schemas

---
+++ migrations/20211031055807_posts_schema.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `posts`;
+DROP TABLE IF EXISTS `posts_ratings`;
+DROP TABLE IF EXISTS `posts_comments`;
+DROP TABLE IF EXISTS `posts_submissions`;
+DROP TABLE IF EXISTS `posts_submissions_ratings`;
