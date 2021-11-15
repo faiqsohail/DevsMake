d99be89ca31038831ecbd8da8fe86acdb4a57524
Faiq Sohail <me@faiqsohail.com>
2021-11-14 23:58:11 -0700

    added placeholder idea card

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { Button } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import NavBar from '../src/NavBar';
 import { basePath } from '../path.config';
+import IdeaCard from '../src/IdeaCard';
 
 export async function getServerSideProps(context) {
   let profile = null;
@@ -34,28 +35,29 @@ export async function getServerSideProps(context) {
 export default function Home({profile}) {
   return (
     <>
-    <NavBar profile={profile} />
-    <section>
-      <Container maxWidth="md">
-        <Box py={8} textAlign="center">
-          <Typography variant="h3" component="h2" gutterBottom={true}>DevsMake</Typography>
-          <Typography variant="h5" color="textSecondary" paragraph={true}>A social platform bringing together users and developers to create amazing projects.</Typography>
-          <Box mt={4}>
-            <Button variant="contained" color="primary" startIcon={<LightbulbIcon />}>submit an idea</Button>
+      <NavBar profile={profile} />
+      <section>
+        <Container maxWidth="md">
+          <Box py={8} textAlign="center">
+            <Typography variant="h3" component="h2" gutterBottom={true}>DevsMake</Typography>
+            <Typography variant="h5" color="textSecondary" paragraph={true}>A social platform bringing together users and developers to create amazing projects.</Typography>
+            <Box mt={4}>
+              <Button variant="contained" color="primary" startIcon={<LightbulbIcon />}>submit an idea</Button>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </section>
-    <Container maxWidth="lg">
-      <Box sx={{ my: 4, width: '100%',
-        height: 300,
-        backgroundColor: 'secondary.main' }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js v5 example
-        </Typography>
-      </Box>
-      <Button variant="contained">Hey</Button>
-    </Container>
+        </Container>
+      </section>
+      <section>
+        <Container maxWidth="lg" sx={{mb: 10}}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          {Array.from(Array(6)).map((_, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <IdeaCard />
+            </Grid>
+          ))}
+        </Grid>
+        </Container>
+      </section>
     </>
   )
 }
---
+++ frontend/src/IdeaCard.js
@@ -0,0 +1,24 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, Typography } from "@mui/material";
+
+export default function IdeaCard() {
+    return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  }
